8e64698348863359e87c9c040df3e911757b40d6
west lepor <westlepor984@gmail.com>
2020-05-20 09:37:40 +0300

    final

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'test',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
